7cacd972e9eb2af1baeba3bb8eb531c8ee82e0e9
wilsont93 <travonnew@gmail.com>
2022-09-08 03:50:33 -0400

    Second Page

--- Projects/Week 00/main.js
+++ Projects/Week 00/main.js
@@ -1,48 +1,42 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
-
-  const User = function (username, password, email, name, dob, uid) {
+function User (username, password, email, name, dob, uid) {
     this.username = username;
     this.password = password;
     this.email = email;
     this.name = name;
     this.dob = dob;
     this.uid = uid;
-  };
-
-  const Post = function (text, uid) {
+  }
+/*   const Post = function (text, uid) {
     this.text = text;
     this.dateCreated = Date.now();
     this.dateUpdated = Date.now();
     this.uid = uid;
-  };
+  }; */
+//var data, users;
+//const usersList = document.getElementById("users")
 
-var data, users, posts;
-const usersList = document.getElementById("users")
-
-console.log(`users: ${users}`);
-console.log(`posts: ${posts}`);
-
-fetch("data.json")
-    .then((response) =>  {
-        return response.json()
-    })
-    .then((fetchedData) => {
-        data = fetchedData
-    })
-    .then(() => {
-        console.log(data)
-        for (let i=0; i < data.users.length; i++) {
-            let currentUser = data.users[i]
-            usersListHTML = usersList.innerHTML
-            usersList.innerHTML = `${usersListHTML}\n <li>${data.users[i].username}</li>
-            <td>${currentUser.username}}</td>
-            <td>${currentUser.name}</td>
-            <td>${currentUser.dob}</td>`
-        }
+ $(document).ready(function (){
+  let endpointurl = "https://twitter-clone-bce1e-default-rtdb.firebaseio.com/";
+  let JSONext = ".json"
+  
+  // AJAX GET
+  $.ajax({
+    type: "GET",
+    dataType: "Json",
+    url: endpointurl+JSONext,
+  success: function (data, status, xhr) {
+    console.log(data)
+}
+ });
+ 
+ // AJAX POST
+ function addUserAPI () {
+ $.ajax({
+    type: "POST",
+    dataType: JSON.stringify(User),
+    url: endpointurl+JSONext,
+    data: userOBJ,
+    success: (data) => console.log(data),
+   })
+  }
     })
